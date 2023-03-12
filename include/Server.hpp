26be70844706a1e0c614281a969deb514b5a6180
jjongs2 <jjongs@hanyang.ac.kr>
2023-03-12 12:16:35 +0900

    feat: Server 클래스에 멤버 변수 추가

--- include/Server.hpp
+++ include/Server.hpp
@@ -1,3 +1,9 @@
+#ifndef SERVER_HPP_
+#define SERVER_HPP_
+
+#include <set>
+#include <string>
+
 class Server {
  public:
   Server();
@@ -7,4 +13,11 @@ class Server {
   Server& operator=(const Server& origin);
 
  private:
+  std::set<int> ports_;
+  std::set<std::string> hosts_;
+  std::set<std::string> server_names_;
+  std::string error_page_;
+  std::size_t body_limit_;
 };
+
+#endif
