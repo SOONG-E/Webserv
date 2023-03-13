c607ad62b23e61930ccb0daaa62bafaf55cb5076
jjongs2 <jjongs@hanyang.ac.kr>
2023-03-13 18:43:34 +0900

    feat: ConfigParser 클래스 구현

--- include/ConfigParser.hpp
+++ include/ConfigParser.hpp
@@ -1,19 +1,34 @@
 #ifndef CONFIG_PARSER_HPP_
 #define CONFIG_PARSER_HPP_
 
-#include <fstream>
-#include <string>
-
 #include "Config.hpp"
-#include "Server.hpp"
 
 class ConfigParser {
  public:
-  static Config parse(const std::ifstream& infile);
+  ConfigParser();
+  explicit ConfigParser(const char* filename);
+  ConfigParser(const ConfigParser& origin);
+  ConfigParser& operator=(const ConfigParser& origin);
+
+  virtual ~ConfigParser();
+
+  Config parse(const char* filename);
 
  private:
-  std::string readFile(const std::ifstream& infile);
-  Server parseServer(str::string content);
+  void parseServerBlock(void);
+  void parseListen(void);
+  void parseServerName(void);
+  void parseErrorPage(void);
+  void parseClientMaxBodySize(void);
+  void parseLocationBlock(void);
+  std::string expect(const std::string& expected);
+  std::string peek(void);
+  void skipWhitespace(void);
+
+  Config config_;
+  Server buffer_;
+  std::string content_;
+  std::size_t pos_;
 };
 
 #endif
--- src/ConfigParser.cpp
+++ src/ConfigParser.cpp
@@ -1,54 +1,110 @@
 #include "ConfigParser.hpp"
 
-#include <queue>
+#include <fstream>
 #include <sstream>
 
 #include "ConfigException.hpp"
 #include "FileOpenException.hpp"
+#include "constant.hpp"
 
-std::string readFile(const std::ifstream& file) {
-  std::stringstream content;
+std::string readFile(const char* filename) {
+  std::ifstream file(filename);
+  if (!file.is_open()) {
+    throw FileOpenException();
+  }
+  std::ostringstream content;
   content << file.rdbuf();
+  file.close();
   return content.str();
 }
 
-std::queue<std::string> split(const std::string& content,
-                              const std::string& separators = " \t\n\v\f\r") {
-  std::queue<std::string> substrings;
-  std::size_t start = 0;
-  std::size_t end;
-  while (true) {
-    end = content.find_first_of(separators);
-    if (end == std::string::npos) break;
-    substrings.push(content.substr(start, end - start));
-    start = end + 1;
+Config ConfigParser::parse(const char* filename) {
+  content_ = readFile(filename);
+  while (peek() == "server") {
+    parseServerBlock();
   }
-  substrings.push(content.substr(start));
-  return substrings;
+  return config_;
 }
 
-Server ConfigParser::parseServer(str::string content) {
-  Server server;
-  std::queue<std::string> lines;
-  lines = split(content, ";");
-  while (!lines.empty()) {
-  }
-}
-
-Config ConfigParser::parse(const std::ifstream& infile) {
-  if (!infile.is_open()) throw FileOpenException();
-  std::string content = readFile(infile);
-  std::queue<std::string> substrings = split(content, "{}");
-  Config config;
-  while (!substrings.empty()) {
-    Server server = parseServer(substrings.front());
-    std::set<std::string> names = server.getServerNames();
-    std::set<std::string>::iterator iter = names.begin();
-    while (iter != names.end()) {
-      config.insertServer(*iter, server);
-      iter++;
+void ConfigParser::parseServerBlock(void) {
+  expect("server");
+  expect("{");
+  while (peek() != "}") {
+    if (peek() == "listen") {
+      parseListen();
+    } else if (peek() == "server_name") {
+      parseServerName();
+    } else if (peek() == "error_page") {
+      parseErrorPage();
+    } else if (peek() == "client_max_body_size") {
+      parseClientMaxBodySize();
+    } else if (peek() == "location") {
+      parseLocationBlock();
+    } else {
+      throw ConfigException(kErrors[kToken]);
     }
-    substrings.pop();
   }
-  return config;
+  expect("}");
+}
+
+void ConfigParser::parseListen(void) {
+  expect("listen");
+  listen_ = expect();
+  expect(";");
+}
+
+void ConfigParser::parseServerName(void) {
+  expect("server_name");
+  while (peek() != ";") {
+    server_names_.insert(expect());
+  }
+  expect(";");
+}
+
+void ConfigParser::parseErrorPage(void) {
+  expect("error_page");
+  std::string code = expect();
+  std::string page = expect();
+  error_pages_.insert(std::make_pair(code, page));
+  expect(";");
+}
+
+void ConfigParser::parseClientBodyLimit(void) {
+  expect("client_max_body_size");
+  body_limit_ = expect();
+  expect(";");
+}
+
+void ConfigParser::parseLocationBlock(void) { expect("location"); }
+
+std::string ConfigParser::expect(const std::string& expected = "") {
+  skipWhitespace();
+  if (expected != "") {
+    if (content_.substr(pos_, expected.size()) != expected) {
+      throw ConfigException(kErrors[kToken]);
+    }
+    pos_ += expected.size();
+    return expected;
+  }
+  std::string token;
+  while (pos_ < content_.size() && !std::isspace(content_[pos_]) &&
+         content_[pos_] != ';' && content_[pos_] != '{' &&
+         content_[pos_] != '}') {
+    token += content_[pos_];
+    pos_ += 1;
+  }
+  return token;
+}
+
+std::string ConfigParser::peek(void) {
+  std::size_t old_pos = pos_;
+  std::string token = expect();
+  pos_ = old_pos;
+  return token;
+}
+
+void ConfigParser::skipWhitespace(void) {
+  while (pos_ < content_.size() && std::isspace(content_[pos_])) {
+    pos_ += 1;
+  }
 }
