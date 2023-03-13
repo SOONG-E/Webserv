da956bccee328ab4e1deef24048fcd4dc6c52241
jjongs2 <jjongs@hanyang.ac.kr>
2023-03-13 18:42:27 +0900

    feat: Location 구조체 추가

--- include/Server.hpp
+++ include/Server.hpp
@@ -1,23 +1,29 @@
 #ifndef SERVER_HPP_
 #define SERVER_HPP_
 
+#include <map>
 #include <set>
 #include <string>
+#include <vector>
+
+struct Location {
+  std::set<std::string> try_files;
+};
 
 class Server {
  public:
   Server();
   Server(const Server& origin);
-  virtual ~Server();
-
   Server& operator=(const Server& origin);
 
+  virtual ~Server();
+
  private:
-  std::set<int> ports_;
-  std::set<std::string> hosts_;
+  std::string listen_;
   std::set<std::string> server_names_;
-  std::string error_page_;
-  std::size_t body_limit_;
+  std::map<std::string, std::string> error_pages_;
+  std::string body_limit_;
+  std::vector<Location> locations_;
 };
 
 #endif
