0168a2bcde0aedde5e90471f5e5745f9f509ef04
jjongs2 <jjongs@hanyang.ac.kr>
2023-03-15 18:14:53 +0900

    feat: Server 클래스 변경
    변경 사항:
    - Server 클래스 내부에 Location 구조체 추가
    - 멤버 함수 추가

--- include/Server.hpp
+++ include/Server.hpp
@@ -6,33 +6,44 @@
 #include <string>
 #include <vector>
 
-struct Location {
-  std::set<std::string> try_files;
-};
-
 class Server {
  public:
+  struct Location;
+
   Server();
   Server(const Server& origin);
   Server& operator=(const Server& origin);
-
   virtual ~Server();
 
-  const std::set<std::string>& getServerNames(void) const;
-
-  void setBodyLimit(const int body_limit);
-
-  void addListen(const std::string& host, const int port);
+  void addListen(const std::string& raw);
   void addServerName(const std::string& name);
   void addErrorPage(const int code, const std::string& page);
+  void setBodyLimit(const std::string& body_limit);
   void addLocation(const Location& location);
 
+  void init(void);
+  std::set<std::string> keys(void) const;
+
  private:
-  std::map<std::string, int> listens_;
+  struct Listen;
+
+  std::vector<Listen> listens_;
   std::set<std::string> server_names_;
   std::map<int, std::string> error_pages_;
-  int body_limit_;
+  std::string body_limit_;
   std::vector<Location> locations_;
+};
+
+struct Server::Listen {
+  explicit Listen(const std::string& raw);
+
+  std::string listen;
+  std::string host;
+  int port;
+};
+
+struct Server::Location {
+  std::set<std::string> try_files;
 };
 
 #endif
--- src/Server.cpp
+++ src/Server.cpp
@@ -1,6 +1,8 @@
 #include "Server.hpp"
 
-#include "ConfigException.hpp"
+#include "constant.hpp"
+#include "exception.hpp"
+#include "utility.hpp"
 
 Server::Server() {}
 
@@ -10,20 +12,15 @@ Server& Server::operator=(const Server& origin) {}
 
 Server::~Server() {}
 
-const std::set<std::string>& Server::getServerNames(void) const {
-  return server_names_;
+void Server::setBodyLimit(const std::string& body_limit) {
+  body_limit_ = body_limit;
 }
 
-void Server::setBodyLimit(const int body_limit) { body_limit_ = body_limit; }
-
-void Server::addListen(const std::string& host, const int port) {
-  listens_.insert(std::make_pair(host, port));
+void Server::addListen(const std::string& listen) {
+  listens_.push_back(Server::Listen(listen));
 }
 
 void Server::addServerName(const std::string& name) {
-  if (server_names_.find(name) != server_names_.end()) {
-    throw ConfigException(kErrors[kToken]);
-  }
   server_names_.insert(name);
 }
 
@@ -33,4 +30,43 @@ void Server::addErrorPage(const int code, const std::string& page) {
 
 void Server::addLocation(const Location& location) {
   locations_.push_back(location);
+}
+
+void Server::init(void) {
+  listens_.clear();
+  server_names_.clear();
+  error_pages_.clear();
+  body_limit_ = kDefaults[kClientMaxBodySize];
+  locations_.clear();
+}
+
+std::set<std::string> Server::keys(void) const {
+  std::set<std::string> keys;
+  std::vector<Listen>::iterator listens_iter = listens_.begin();
+  std::vector<Listen>::iterator listens_end = listens_.end();
+  std::set<std::string>::iterator names_iter;
+  std::set<std::string>::iterator names_end = server_names_.end();
+  while (listens_iter != listens_end) {
+    names_iter = server_names_.begin();
+    while (names_iter != names_end) {
+      keys.insert(*listens_iter + ":" + *names_iter);
+      ++names_iter;
+    }
+    ++listens_iter;
+  }
+  return keys;
+}
+
+Server::Listen::Listen(const std::string& raw)
+    : listen(raw), host(kDefaults[kHost]) {
+  if (raw == "") return;
+  std::vector<std::string> splitted = split(raw, ":");
+  if (splitted.size() == 1) {
+    port = stoi(splitted[0]);
+  } else if (splitted.size() == 2) {
+    host = listen[0];
+    port = stoi(splitted[1]);
+  } else {
+    throw ConfigException(kErrors[kToken]);
+  }
 }
