f8f68d1d3e6b366c6a410b6836a95e490a98611e
jjongs2 <jjongs@hanyang.ac.kr>
2023-03-15 18:26:05 +0900

    feat: 기본값 관련 문자열 상수 추가

--- include/constant.hpp
+++ include/constant.hpp
@@ -3,6 +3,10 @@
 
 #include <string>
 
+enum DefaultIndex {
+  kHost,
+  kClientMaxBodySize,
+};
 enum ErrorIndex {
   kPrefix,
   kArg,
@@ -10,8 +14,10 @@ enum ErrorIndex {
   kToken,
 };
 
+const int kNumOfDefaults = 2;
 const int kNumOfErrors = 4;
 
+const std::string kDefaults[kNumOfDefaults] = {"0.0.0.0", "1m"};
 const std::string kErrors[kNumOfErrors] = {
     "Error: ", "Invalid argument", "File open failed", "Unexpected token"};
 
