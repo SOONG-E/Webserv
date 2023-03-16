1f0c4ca079b8e35ebb91cf49f344e657d8ed79a2
zerone015 <zerone015@naver.com>
2023-03-16 15:39:33 +0900

    feat: Client 클래스 추가

---
+++ Client.cpp
@@ -0,0 +1,19 @@
+#include "Client.hpp"
+
+Client::Client(int socket, const InetSocketAddress& serv_addr) {
+  _socket = socket;
+  _server_info_key = serv_addr.getIP() + ":" + serv_addr.getPort();
+}
+
+Client::Client(const Client& src) { *this = src; }
+
+Client::~Client() {}
+
+Client& Client::operator=(const Client& src) {
+  _socket = src._socket;
+  _server_info_key = src._server_info_key;
+}
+
+int Client::getSocket() const { return _socket; }
+
+const std::string& Client::getKey() const { return _server_info_key; }
---
+++ Client.hpp
@@ -0,0 +1,24 @@
+#ifndef CLIENT_HPP
+#define CLIENT_HPP
+
+#include <string>
+
+#include "InetSocketAddress.hpp"
+
+class Client {
+ public:
+  Client(int socket, const InetSocketAddress& address);
+  Client(const Client& src);
+  ~Client();
+
+  Client& operator=(const Client& src);
+
+  int getSocket() const;
+  const std::string& getKey() const;
+
+ private:
+  int _socket;
+  std::string _server_info_key;
+};
+
+#endif
