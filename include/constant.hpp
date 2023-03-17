5265475dde19043c614ecc79873dc52e8a363938
Youngwon Son <yoson@c7r3s2.42seoul.kr>
2023-03-17 21:36:59 +0900

    chore: methods 변수 constant 헤더로 이동

--- include/constant.hpp
+++ include/constant.hpp
@@ -15,12 +15,12 @@ enum ErrorIndex {
   kToken,
 };
 
-const int kNumOfDefaults = 3;
-const int kNumOfErrors = 4;
-
 const std::string kBase10 = "0123456789";
-const std::string kDefaults[kNumOfDefaults] = {"0.0.0.0", "80", "1m"};
-const std::string kErrors[kNumOfErrors] = {
-    "Error: ", "Invalid argument", "File open failed", "Unexpected token"};
+const std::string kDefaults[] = {"0.0.0.0", "80", "1m"};
+const std::string kErrors[] = {"Error: ", "Invalid argument",
+                               "File open failed", "Unexpected token"};
+
+const std::string methods[8] = {"OPTIONS", "GET",    "HEAD",  "POST",
+                                "PUT",     "DELETE", "TRACE", "CONNECT"};
 
 #endif
