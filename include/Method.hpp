b034a150153326a845941e0d1fd6cefc5dc1cfa3
Youngwon Son <yoson@c7r3s2.42seoul.kr>
2023-03-17 21:33:43 +0900

    feat: change file name, move method variable to constant.hpp

--- include/Method.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef METHOD_HPP_
-#define METHOD_HPP_
-
-#include <string>
-
-const std::string methods[8] = {"OPTIONS", "GET",    "HEAD",  "POST",
-                                "PUT",     "DELETE", "TRACE", "CONNECT"};
-
-#endif
