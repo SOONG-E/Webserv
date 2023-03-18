246369eac11b4f15aa8b3bf664a72a1dbd2ff26a
zerone015 <zerone015@naver.com>
2023-03-18 22:21:06 +0900

    chore: Client 클래스 getKey함수 추가 및 멤버 변수 구조 변경

--- include/Client.hpp
+++ include/Client.hpp
@@ -1,26 +1,46 @@
 #ifndef CLIENT_HPP
 #define CLIENT_HPP
 
+#define BUF_SIZE 65536
+
 #include <string>
 
 #include "HttpParser.hpp"
-#include "HttpRequest.hpp"
-#include "InetSocketAddress.hpp"
+#include "ServerBlock.hpp"
+#include "SocketAddress.hpp"
 
 class Client {
  public:
-  Client(int socket, const InetSocketAddress& address);
+  Client(int socket, const SocketAddress& cli_addr,
+         const SocketAddress& serv_addr);
   Client(const Client& src);
   ~Client();
 
   Client& operator=(const Client& src);
 
   int getSocket() const;
+  HttpParser& getParser();
+  std::string getKey() const;
+  void setServerBlock(const ServerBlock* server_block);
+  std::string receive() const;
 
  private:
   int _socket;
-  InetSocketAddress _address;
-  HttpRequest _request;
+  SocketAddress _cli_addr;
+  SocketAddress _serv_addr;
+  HttpParser _parser;
+  const ServerBlock* _server_block;
+
+  // exception
+  class SocketReceiveException : public std::exception {
+   public:
+    SocketReceiveException(const char* cause);
+
+   private:
+    const char* cause;
+
+    const char* what() const throw();
+  };
 };
 
 #endif
--- src/Client.cpp
+++ src/Client.cpp
@@ -1,9 +1,11 @@
 #include "Client.hpp"
 
-Client::Client(int socket, const InetSocketAddress& address) {
-  _socket = socket;
-  _address = address;
-}
+Client::Client(int socket, const SocketAddress& cli_addr,
+               const SocketAddress& serv_addr)
+    : _socket(socket),
+      _cli_addr(cli_addr),
+      _serv_addr(serv_addr),
+      _server_block(NULL) {}
 
 Client::Client(const Client& src) { *this = src; }
 
@@ -11,10 +13,38 @@ Client::~Client() {}
 
 Client& Client::operator=(const Client& src) {
   _socket = src._socket;
-  _address = src._address;
-  _request = src._request;
+  _cli_addr = src._cli_addr;
+  _serv_addr = src._serv_addr;
+  _parser = src._parser;
+  _server_block = src._server_block;
 
   return *this;
 }
 
 int Client::getSocket() const { return _socket; }
+
+HttpParser& Client::getParser() { return _parser; }
+
+std::string Client::getKey() const {
+  return _serv_addr.getIP() + ":" + _serv_addr.getPort();
+}
+
+void Client::setServerBlock(const ServerBlock* server_block) {
+  _server_block = server_block;
+}
+
+std::string Client::receive() const {
+  char buf[BUF_SIZE] = {0};
+
+  ssize_t read_bytes = recv(_socket, &buf, BUF_SIZE, 0);
+  if (read_bytes == -1) throw SocketReceiveException(strerror(errno));
+
+  return std::string(buf, static_cast<size_t>(read_bytes));
+}
+
+Client::SocketReceiveException::SocketReceiveException(const char* cause)
+    : cause(cause) {}
+
+const char* Client::SocketReceiveException::what() const throw() {
+  return cause;
+}
