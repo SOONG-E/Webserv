84301cbd311b99be2c17168b47f01eb70ffcafb7
Youngwon Son <yoson@c7r3s5.42seoul.kr>
2023-03-19 20:07:03 +0900

    feat: 상수 추가 및 변경

--- include/ByteUnits.hpp
+++ include/ByteUnits.hpp
@@ -6,31 +6,31 @@
 
 struct ByteUnits {
   enum Index {
-    kLowerKilo,
-    kUpperKilo,
-    kLowerMega,
-    kUpperMega,
-    kLowerGiga,
-    kUpperGiga,
+    LOWER_KILO,
+    UPPER_KILO,
+    LOWER_MEGA,
+    UPPER_MEGA,
+    LOWER_GIGA,
+    UPPER_GIGA,
   };
 
-  static const std::string kKeys[];
-  static const std::size_t kValues[];
+  static const std::string KEYS[];
+  static const std::size_t VALUES[];
 
   ByteUnits() {
-    size.insert(std::make_pair(kKeys[kLowerKilo], kValues[kLowerKilo]));
-    size.insert(std::make_pair(kKeys[kUpperKilo], kValues[kUpperKilo]));
-    size.insert(std::make_pair(kKeys[kLowerMega], kValues[kLowerMega]));
-    size.insert(std::make_pair(kKeys[kUpperMega], kValues[kUpperMega]));
-    size.insert(std::make_pair(kKeys[kLowerGiga], kValues[kLowerGiga]));
-    size.insert(std::make_pair(kKeys[kUpperGiga], kValues[kUpperGiga]));
+    size.insert(std::make_pair(KEYS[LOWER_KILO], VALUES[LOWER_KILO]));
+    size.insert(std::make_pair(KEYS[UPPER_KILO], VALUES[UPPER_KILO]));
+    size.insert(std::make_pair(KEYS[LOWER_MEGA], VALUES[LOWER_MEGA]));
+    size.insert(std::make_pair(KEYS[UPPER_MEGA], VALUES[UPPER_MEGA]));
+    size.insert(std::make_pair(KEYS[LOWER_GIGA], VALUES[LOWER_GIGA]));
+    size.insert(std::make_pair(KEYS[UPPER_GIGA], VALUES[UPPER_GIGA]));
   }
 
   std::map<std::string, std::size_t> size;
 };
 
-const std::string ByteUnits::kKeys[] = {"k", "K", "m", "M", "g", "G"};
-const std::size_t ByteUnits::kValues[] = {1 << 10, 1 << 10, 1 << 20,
-                                          1 << 20, 1 << 30, 1 << 30};
+const std::string ByteUnits::KEYS[] = {"k", "K", "m", "M", "g", "G"};
+const std::size_t ByteUnits::VALUES[] = {1 << 10, 1 << 10, 1 << 20,
+                                         1 << 20, 1 << 30, 1 << 30};
 
 #endif
