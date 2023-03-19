a8836be8da62c152bb421515ea4968c38748d9c6
Youngwon Son <yoson@c7r3s5.42seoul.kr>
2023-03-19 20:14:51 +0900

    feat: LocationBlock 구조체에 기본 생성자 및 상수 추가

--- include/LocationBlock.hpp
+++ include/LocationBlock.hpp
@@ -5,6 +5,21 @@
 #include <string>
 
 struct LocationBlock {
+  enum Index {
+    ROOT,
+    AUTOINDEX,
+    INDEX,
+  };
+
+  static const std::string DEFAULTS[];
+
+  LocationBlock() : root(DEFAULTS[ROOT]), autoindex(DEFAULTS[AUTOINDEX]) {
+    for (int i = 0; i < METHODS_COUNT; ++i) {
+      allowed_methods.insert(METHODS[i]);
+    }
+    index.insert(DEFAULTS[INDEX]);
+  }
+
   std::string uri;
   std::set<std::string> allowed_methods;
   std::string return_code;
@@ -12,7 +27,9 @@ struct LocationBlock {
   std::string root;
   std::string autoindex;
   std::set<std::string> index;
-  std::set<std::string> try_files;
+  // std::set<std::string> try_files;
 };
+
+const std::string LocationBlock::DEFAULTS[] = {"html", "off", "index.html"};
 
 #endif
