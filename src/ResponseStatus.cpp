6b46c789c55e0c9a670e391c5eed23ae157ffe6c
Youngwon Son <yoson@c7r9s1.42seoul.kr>
2023-03-25 20:29:26 +0900

    fix: 콤마 누락 수정

--- src/ResponseStatus.cpp
+++ src/ResponseStatus.cpp
@@ -1,14 +1,7 @@
 #include "ResponseStatus.hpp"
 
-const std::string ResponseStatus::CODES[] = {"200",
-                                             "400",
-                                             "404",
-                                             "405"
-                                             "411",
-                                             "413",
-                                             "500",
-                                             "501",
-                                             "505"};
+const std::string ResponseStatus::CODES[] = {"200", "400", "404", "405", "411",
+                                             "413", "500", "501", "505"};
 
 const std::string ResponseStatus::REASONS[] = {
     "OK",                           // 200
