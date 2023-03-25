4651bd99e395a637ad55c4a3fbcc244d6ff13324
Youngwon Son <yoson@c7r9s1.42seoul.kr>
2023-03-25 18:02:40 +0900

    chore: 불필요한 코드 제거

--- include/ResponseStatus.hpp
+++ include/ResponseStatus.hpp
@@ -35,24 +35,4 @@ struct ResponseStatus {
   std::map<int, std::string> reason;
 };
 
-const std::string ResponseStatus::CODES[] = {"200",
-                                             "400",
-                                             "404",
-                                             "405"
-                                             "411",
-                                             "413",
-                                             "500",
-                                             "501",
-                                             "505"};
-
-const std::string ResponseStatus::REASONS[] = {"OK",
-                                               "Bad Request",
-                                               "Not Found",
-                                               "Method Not Allowed",
-                                               "Length Required",
-                                               "Payload Too Large",
-                                               "Internal Server Error",
-                                               "Not Implement",
-                                               "HTTP Version Not Supported"};
-
 #endif
