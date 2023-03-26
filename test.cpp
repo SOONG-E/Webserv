f61408b3339dc0ae83847b9fe88e0cf74b2b0b4b
soong-e <sksuwjd@naver.com>
2023-03-26 19:50:27 +0900

    test: upload test file

--- test.cpp
+++ test.cpp
@@ -6,26 +6,34 @@
 #include <map>
 #include <string>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
   std::map<std::string, std::string> map;
   map["REQUEST_METHOD"] = "GET";
   map["SERVER_PROTOCOL"] = "HTTP/1.1";
   map["PATH_INFO"] = "/Users/yoson/Desktop/Webserv/cgi-bin/index.php";
   map["REQUEST_URI"] = "/Users/yoson/Desktop/Webserv/cgi-bin/index.php";
   map["SCRIPT_NAME"] = "/Users/yoson/Desktop/Webserv/cgi-bin/index.php";
+  map["NAME"] = "asdasd";
 
-  char** envp = new char*[map.size() + 1]();
+  char *pythonPath = "/usr/bin/python3";
+  char *pythonScriptPath = "./test.py";
+  char *args[] = {pythonPath, pythonScriptPath, "NAME=qwe;FIELD=qqqq", NULL};
+
+  char **envp = new char *[map.size() + 1]();
   int i = 0;
   for (std::map<std::string, std::string>::const_iterator it = map.begin();
        it != map.end(); ++it) {
-    envp[i] = strdup(const_cast<char*>((it->first + "=" + it->second).c_str()));
+    envp[i] =
+        strdup(const_cast<char *>((it->first + "=" + it->second).c_str()));
     ++i;
   }
   envp[i] = NULL;
   pid_t pid = fork();
   if (pid == 0) {
-    execve("./cgi_tester", NULL, envp);
+    std::cout << "FORK" << std::endl;
+    execve(pythonPath, args, envp);
   } else {
+    std::cout << "qqq" << std::endl;
     wait(0);
   }
   return EXIT_SUCCESS;
