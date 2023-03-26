acced1d323de323536d0d33d16ab1816e8ecb756
soong-e <sksuwjd@naver.com>
2023-03-26 14:58:22 +0900

    feat: add implemented methods

--- include/constant.hpp
+++ include/constant.hpp
@@ -10,7 +10,7 @@ enum ErrorIndex {
   TOKEN,
 };
 
-const int METHODS_COUNT = 3;
+const int METHODS_COUNT = 8;
 const std::size_t HEADER_MAX_SIZE = 8192;
 
 const std::string BASE10 = "0123456789";
@@ -19,6 +19,7 @@ const std::string DOUBLE_CRLF = "\r\n\r\n";
 const std::string WHITESPACE = " \t\n\v\f\r";
 const std::string ERRORS[] = {"Error: ", "Invalid argument", "File open failed",
                               "Unexpected token"};
-const std::string METHODS[] = {"GET", "POST", "DELETE"};
+const std::string METHODS[] = {"GET", "POST",    "DELETE", "HEAD",
+                               "PUT", "OPTIONS", "TRACE",  "CONNECT"};
 
 #endif
