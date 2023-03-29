a87ed5d75827c4263bd12c63adb98ef650446eb4
zerone015 <zerone015@naver.com>
2023-03-30 01:47:28 +0900

    chore: Selector 클래스 예외 삭제 및 복사 생성자에 tm_ 멤버 대입 추가

--- src/Selector.cpp
+++ src/Selector.cpp
@@ -17,6 +17,7 @@ Selector& Selector::operator=(const Selector& src) {
     fds_ = src.fds_;
     read_fds_ = src.read_fds_;
     write_fds_ = src.write_fds_;
+    tm_ = src.tm_;
   }
   return *this;
 }
@@ -26,23 +27,18 @@ Selector::~Selector() {}
 int Selector::select() {
   read_fds_ = fds_;
   write_fds_ = fds_;
-  int result = ::select(max_fd_ + 1, &read_fds_, &write_fds_, 0, &tm_);
-  if (result == -1) throw SelectFailedException(strerror(errno));
-  return result;
+
+  return ::select(max_fd_ + 1, &read_fds_, &write_fds_, 0, &tm_);
 }
 
 void Selector::registerFD(int fd) {
   FD_SET(fd, &fds_);
-  if (fd > max_fd_) max_fd_ = fd;
+  if (fd > max_fd_) {
+    max_fd_ = fd;
+  }
 }
+
 void Selector::clear(int fd) { FD_CLR(fd, &fds_); }
 
 bool Selector::isReadable(int fd) const { return FD_ISSET(fd, &read_fds_); }
 bool Selector::isWritable(int fd) const { return FD_ISSET(fd, &write_fds_); }
-
-Selector::SelectFailedException::SelectFailedException(const char* cause)
-    : cause(cause) {}
-
-const char* Selector::SelectFailedException::what() const throw() {
-  return cause;
-}
