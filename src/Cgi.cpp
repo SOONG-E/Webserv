797440d5d29f2a7b29a980cdf26b6c937087334f
jjongs2 <jjongs@hanyang.ac.kr>
2023-04-03 21:01:46 +0900

    fix: compile error 수정

--- src/Cgi.cpp
+++ src/Cgi.cpp
@@ -169,7 +169,7 @@ char** Cgi::generateEnvp(const HttpRequest& request_obj,
   env_map["SERVER_SOFTWARE"] = "webserv/1.1";
   env_map["HTTP_X_SERVER_KEY"] =
       toString(response_obj.getServerBlock()->getKey());
-  env_map["HTTP_X_SESSION_ID"] = response_obj.getSession().getID();
+  env_map["HTTP_X_SESSION_ID"] = response_obj.getSession()->getID();
   env_map["HTTP_X_SECRET_HEADER_FOR_TEST"] =
       request_obj.getHeader("X-SECRET-HEADER-FOR-TEST");
 
