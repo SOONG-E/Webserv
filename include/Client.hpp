147213da267410c696d8d2c85143b12e123ac6da
soong-e <yujelee@student.42seoul.kr>
2023-04-16 16:44:10 +0900

    chore: reser Client class

--- include/Client.hpp
+++ include/Client.hpp
@@ -5,62 +5,27 @@
 #include <exception>
 #include <string>
 
-#include "Cgi.hpp"
+#include "HttpRequest.hpp"
+#include "TcpServer.hpp"
 
 class Client {
  public:
-  Client(int fd, const ServerBlock& default_server,
-         const SocketAddress& cli_addr, const SocketAddress& serv_addr);
-  Client(const Client& src);
+  Client(const int fd, const TcpServer* tcp_server);
+  Client(const Client& origin);
   ~Client();
 
-  int getFD() const;
-  std::string getServerSocketKey() const;
-  int getServerBlockKey() const;
-  std::string getSessionKey() const;
-  HttpRequest& getRequest();
-  const HttpRequest& getRequest() const;
-  HttpResponse& getResponse();
-  const HttpResponse& getResponse() const;
-  Cgi& getCgi();
-  const Cgi& getCgi() const;
-  const SocketAddress& getServerAddress() const;
-  const SocketAddress& getClientAddress() const;
-  std::time_t getTimeout() const;
-
-  void setTimeout(std::time_t time = std::time(NULL));
-  void setSessionTimeout();
-
-  std::string receive() const;
-  void send();
-  void runCgiProcess(Selector& selector);
-  void executeCgiIO(Selector& selector);
-  void closeConnection() const;
-  void clear();
-
-  bool isCgi() const;
-  bool isPartialResponse() const;
-  bool isReadyToCgiIO() const;
-  bool isReadyToSend() const;
-
-  void logAddressInfo() const;
-  void logConnectionInfo() const;
-  void logReceiveInfo(const std::string& request) const;
-
-  class ConnectionClosedException : public std::exception {
-   public:
-    const char* what() const throw();
-  };
+  int getFd(void) const;
+  HttpServer* getHttpServer(void) const;
+  HttpRequest getRequest(void) const;
+  std::string getResponse(void) const;
 
  private:
-  int fd_;
-  SocketAddress cli_address_;
-  SocketAddress serv_address_;
+  const int fd_;
+  const TcpServer* tcp_server_;
+  HttpServer* http_server_;
   HttpRequest request_;
-  HttpResponse response_;
-  Cgi cgi_;
-  std::string buf_;
-  std::time_t timeout_;
+  std::string response_;
+  std::string buffer_;
 };
 
 #endif
