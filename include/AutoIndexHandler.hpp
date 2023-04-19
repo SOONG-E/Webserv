76e76fc2324244f4aabfcdd46e5ca4f73c9d6c76
soong-e <sksuwjd@naver.com>
2023-04-19 18:16:27 +0900

    feat: AutoIndexHandler 초안 작성

--- include/AutoIndexHandler.hpp
+++ include/AutoIndexHandler.hpp
@@ -1,6 +1,23 @@
 #ifndef AUTO_INDEX_HANDLER_HPP_
 #define AUTO_INDEX_HANDLER_HPP_
 
-class AutoIndexHandler {};
+#include <dirent.h>
+#include <sys/stat.h>
+
+#include "Client.hpp"
+#include "File.hpp"
+
+class AutoIndexHandler {
+ public:
+  static struct Response handle(Client *client);
+
+ private:
+  AutoIndexHandler(){};
+  ~AutoIndexHandler(){};
+
+  static std::string generateBody(Client *client);
+  static std::set<File> getEntries(const std::string &url);
+  static std::string generateList(const std::set<File> &entries);
+};
 
 #endif
--- src/handler/AutoIndexHandler.cpp
+++ src/handler/AutoIndexHandler.cpp
@@ -1,1 +1,67 @@
 #include "AutoIndexHandler.hpp"
+
+struct Response AutoIndexHandler::handle(Client *client) {
+  struct Response response;
+
+  response.body = generateBody(client);
+  response.headers["content-length"] = toString(response.body.size());
+
+  return response;
+}
+
+std::string AutoIndexHandler::generateBody(Client *client) {
+  std::set<File> entries = getEntries(client->getFullUri());
+  return generateList(entries);
+}
+
+std::set<File> AutoIndexHandler::getEntries(const std::string &url) {
+  DIR *dir = opendir(url.c_str());
+  if (!dir) {
+    throw std::runtime_error(strerror(errno));
+  }
+  std::set<File> entries;
+  struct dirent *entry;
+  while ((entry = readdir(dir))) {
+    std::string name = entry->d_name;
+    if (name == "." || name == "..") continue;
+    struct stat statbuf;
+    if (stat((url + name).c_str(), &statbuf) == ERROR<int>()) {
+      throw std::runtime_error(strerror(errno));
+    }
+    File file = {name, statbuf.st_mtime, statbuf.st_size};
+    entries.insert(file);
+  }
+  closedir(dir);
+
+  return entries;
+}
+
+std::string AutoIndexHandler::generateList(const std::set<File> &entries) {
+  std::string listing = ::readFile(DIRECTORY_LISTING_PAGE);
+  std::string delimeter = "</tr>\n";
+
+  std::size_t boundary = listing.find(delimeter);
+  if (boundary == std::string::npos) {
+    throw std::runtime_error("");  // error handling
+  }
+  std::string head = listing.substr(0, boundary);
+  std::string tail = listing.substr(boundary + delimeter.size());
+
+  for (std::set<File>::const_iterator entry_iter = entries.begin();
+       entry_iter != entries.end(); ++entry_iter) {
+    head +=
+        "      <tr>"
+        "        <td>" +
+        entry_iter->name +
+        "        </td>"
+        "        <td>" +
+        formatTime("%Y-%m-%d %H:%M:%S", entry_iter->last_modified) +
+        "        </td>"
+        "        <td>" +
+        toString(entry_iter->size) +
+        "        </td>"
+        "      </tr>";
+  }
+  head += tail;
+  return head;
+}
