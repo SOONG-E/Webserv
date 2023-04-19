f1e173ef59f78e1584cc67f3c0c7bcf3f4e7d660
soong-e <sksuwjd@naver.com>
2023-04-19 17:01:21 +0900

    feat: StaticContentHandler 초안 작성

--- include/StaticContentHandler.hpp
+++ include/StaticContentHandler.hpp
@@ -1,6 +1,18 @@
 #ifndef STATIC_CONTENT_HANDLER_HPP_
 #define STATIC_CONTENT_HANDLER_HPP_
 
-class StaticContentHandler {};
+#include "Client.hpp"
+
+class StaticContentHandler {
+ public:
+  static struct Response handle(Client *client);
+
+ private:
+  StaticContentHandler(){};
+  ~StaticContentHandler(){};
+
+  static std::string generateBody(Client *client);
+  static std::string loadErrorPage(HttpServer *http_server, const int status);
+};
 
 #endif
--- src/handler/StaticContentHandler.cpp
+++ src/handler/StaticContentHandler.cpp
@@ -1,1 +1,41 @@
 #include "StaticContentHandler.hpp"
+
+struct Response StaticContentHandler::handle(Client *client) {
+  struct Response response;
+
+  response.body = generateBody(client);
+  response.has_body = true;
+
+  response.headers["content-length"] = toString(response.body.size());
+
+  return response;
+}
+
+std::string StaticContentHandler::generateBody(Client *client) {
+  std::string body;
+  if (client->isErrorCode() == true) {
+    body = loadErrorPage(client->getHttpServer(), client->getStatusInt());
+    return body;
+  }
+  std::string uri = client->getFullUri();
+  if (*uri.rbegin() == '/') {
+    uri.pop_back();  // 수정!!!!
+  }
+  try {
+    body = ::readFile(uri);
+  } catch (FileOpenException &e) {
+    client->setStatus(C404);
+    throw;  // error handling
+  } catch (std::exception &e) {
+    throw;
+  }
+  return body;
+}
+
+std::string StaticContentHandler::loadErrorPage(HttpServer *http_server,
+                                                const int status) {
+  std::string url;
+  std::string uri = http_server->getErrorPage(ResponseStatus::CODES[status]);
+
+  return ::readFile(uri);
+}
