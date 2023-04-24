aa777c4b32e3fcf760f7a90c61de9e0a60e926b5
soong-e <sksuwjd@naver.com>
2023-04-24 15:54:46 +0900

    refactor: handler 폴더 생성

--- include/CgiHandler.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef CGI_HANDLER_HPP_
-#define CGI_HANDLER_HPP_
-
-class CgiHandler {};
-
-#endif
---
+++ include/handler/CgiHandler.hpp
@@ -0,0 +1,11 @@
+#ifndef CGI_HANDLER_HPP_
+#define CGI_HANDLER_HPP_
+
+class CgiHandler {
+ public:
+ private:
+  CgiHandler();
+  ~CgiHandler();
+};
+
+#endif
